ecc17cc9bc0d85b5ad5299ba051ad4bee48891c9
aktaylo4 <aktaylo4@ncsu.edu>
2024-01-14 21:35:32 -0500

    Testing

--- hw02/src/test.py
+++ hw02/src/test.py
@@ -1,1 +1,25 @@
+from num import NUM
+class TestNUM:
+    def __init__(self):
+        self.num_instance = NUM("a", 5)
+        self.run_tests()
+    
+    def run_tests(self):
+        self.test_initialization()
+        print("PASS")
 
+    ## Helper function
+    def assert_equal(self, actual, expected):
+        assert actual == expected, f"Expected {expected}, but got {actual}"
+
+    def test_initialization(self):
+        self.assert_equal(self.num_instance.s, "a")
+        self.assert_equal(self.num_instance.at, 5)
+        self.assert_equal(self.num_instance.n, 0)
+        self.assert_equal(self.num_instance.mu, 0)
+        self.assert_equal(self.num_instance.m2, 0)
+        self.assert_equal(self.num_instance.hi, float('-inf'))
+        self.assert_equal(self.num_instance.lo, float('inf'))
+        self.assert_equal(self.num_instance.heaven, 1)
+
+TestNUM()
