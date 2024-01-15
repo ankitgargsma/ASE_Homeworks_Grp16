904f24210159659da17f3bc25ff47231bca776ab
aktaylo4 <aktaylo4@ncsu.edu>
2024-01-14 22:52:40 -0500

    Gate

--- hw02/src/gate.py
+++ hw02/src/gate.py
@@ -6,8 +6,15 @@ import sys
 class gate:
     def run(fileFlag, testFlag):
         if (testFlag== "stats"):
-            Tests.stats(fileFlag)
+            test = Tests()
+            test.stats(fileFlag)
 
 
+i= 0
 for arg in sys.argv:
-    print(arg)
+    if(arg == "-f"):
+        fileFlag = sys.argv[i+1]
+    if(arg == "-t"):
+        testFlag = sys.argv[i+1]
+    i = i+ 1
+gate.run(fileFlag, testFlag)
