b6f2da1790defdd3cbe5b1109af6ec6cfc660624
aktaylo4 <aktaylo4@ncsu.edu>
2024-01-14 21:38:23 -0500

    tests

--- hw02/src/test.py
+++ hw02/src/test.py
@@ -1,5 +1,5 @@
 from num import NUM
-class TestNUM:
+class Tests:
     def __init__(self):
         self.num_instance = NUM("a", 5)
         self.run_tests()
@@ -22,4 +22,18 @@ class TestNUM:
         self.assert_equal(self.num_instance.lo, float('inf'))
         self.assert_equal(self.num_instance.heaven, 1)
 
-TestNUM()
+    def test_add_method(self):
+        self.num_instance.add(10, 2)
+        self.assert_equal(self.num_instance.n, 1)
+        self.assert_equal(self.num_instance.mu, 10)
+        self.assert_equal(self.num_instance.m2, 0)
+        self.assert_equal(self.num_instance.lo, 10)
+        self.assert_equal(self.num_instance.hi, 10)
+
+        self.num_instance.add(20, 3)
+        self.assert_equal(self.num_instance.n, 2)
+        self.assert_equal(self.num_instance.mu, 15)
+        self.assert_equal(self.num_instance.m2, 50)
+        self.assert_equal(self.num_instance.lo, 10)
+        self.assert_equal(self.num_instance.hi, 20)
+Tests()
