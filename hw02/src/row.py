248ddf177dad3b98451baeef97c0a12ecb958712
aktaylo4 <aktaylo4@ncsu.edu>
2024-01-19 19:53:10 -0500

    ROW like function

--- hw02/src/row.py
+++ hw02/src/row.py
@@ -1,3 +1,18 @@
+import math
+import utils
+
 class ROW:
     def __init__(self, t):
         self.cells = t
+
+    def like(self, data, n, nHypotheses):
+        prior = (len(data.rows) + utils.THE_K) / (n + utils.THE_K * nHypotheses)
+        out = math.log(prior)
+        
+        for col in data.cols.x:
+            v = self.cells[col.at]
+            if v != "?":
+                inc = col.like(v, prior)
+                out += math.log(inc)
+
+        return math.exp(out)
