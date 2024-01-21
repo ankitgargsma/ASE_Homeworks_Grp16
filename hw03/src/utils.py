91de1f9c3f76880dde8b7cac0c2e8a96b7880524
Ankit Garg <ankitgargsma@gmail.com>
2024-01-21 15:57:23 -0500

    Added settings in utils

--- hw03/src/utils.py
+++ hw03/src/utils.py
@@ -45,3 +45,28 @@ def csv(src):
         s = src.readline().strip()
     src.close()
     return
+
+def settings(s):
+    t = {}
+    opt_dir = {}
+    opts = re.findall(r'-(\w+)\s+--(\w+)\s+.*=\s*(\S+)', s)
+    for short_form, full_form, default_value in opts:
+        t[full_form] = coerce(default_value)
+        opt_dir[short_form] = full_form
+
+    options = sys.argv[1:]
+    if "--help" in options or "-h" in options:
+        t["help"] = True
+        return t
+
+    options_dict = {}
+    for i in range(0, len(options), 2):
+        opt = options[i]
+        val = options[i + 1] if i + 1 < len(options) else None
+        options_dict[opt] = val
+
+    for opt, val in options_dict.items():
+        key = opt[2:] if opt.startswith('--') else opt_dir[opt[1:]]
+        t[key] = coerce(val)
+
+    return t
