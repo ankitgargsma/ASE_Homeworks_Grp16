f07c23193f5028dfb7ccc2687580a8e9de5c7917
Rhythm Jagota <rythmjagota2@gmail.com>
2024-01-21 16:09:35 -0500

    Added the learn and bayes function

--- hw03/src/gate.py
+++ hw03/src/gate.py
@@ -23,6 +23,23 @@ class gate:
         else:
             print(f"Unknown test: {test_name}")
 
+    def learn(data, row, my, n_hypotheses, most, tmp, out):
+            my['n'] += 1
+            kl = row.cells[data.cols.klass.at]
+            if my['n'] > 10:
+                my['tries'] += 1
+                my['acc'] += 1 if kl == row.likes(my['datas'], my['n'], n_hypotheses, most, tmp, out)[0] else 0
+            my['datas'][kl] = my['datas'].get(kl, DATA(data.cols.names))
+            my['datas'][kl].add(row)
+
+    def bayes():
+            wme = {'acc': 0, 'datas': {}, 'tries': 0, 'n': 0}
+            n_hypotheses, most, tmp, out = 0, None, None, None 
+            DATA(t['file'], lambda data, t: learn(data, t, wme, n_hypotheses, most, tmp, out))
+            accuracy = wme['acc'] / wme['tries']
+            print(accuracy)
+            return accuracy
+
 if __name__ == "__main__":
     fileFlag, testFlag = None, None
 
