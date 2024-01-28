9da592f0ab52c6353193e97f131290b3c9109333
aktaylo4 <aktaylo4@ncsu.edu>
2024-01-27 20:38:04 -0500

    Split

--- hw04/src/data.py
+++ hw04/src/data.py
@@ -55,4 +55,20 @@ class DATA:
             else:
                 rest.append(row)
 
-    #def split(best, rest, lite, dark):
+    def split(self, best, rest, lite, dark):
+        selected = DATA([self.cols.names])
+        max_val = 1E30
+        out = 1
+
+        for i, row in enumerate(dark, 1):
+            b = row.like(best, len(lite), 2)
+            r = row.like(rest, len(lite), 2)
+            
+            if b > r:
+                selected.add(row)
+            
+            tmp = abs(b + r) / abs(b - r + 1E-300)
+            if tmp > max_val:
+                out, max_val = i, tmp
+
+        return out, selected
